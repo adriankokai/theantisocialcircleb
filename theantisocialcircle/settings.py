40505c6381942d6a70db50555f5eff6f9084d0b9
Adrian Kokai <adriankokai@gmail.com>
2021-06-21 22:20:58 +0200

    change settings

--- theantisocialcircle/settings.py
+++ theantisocialcircle/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from dotenv import load_dotenv
+##from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
 import os
 
-load_dotenv()
+##load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
